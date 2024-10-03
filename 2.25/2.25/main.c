3bb7248b69ad9de5af6bae47dcbac7e3e096d416
113360222 <t113360222@ntut.edu.tw>
2024-10-03 22:13:46 +0800

    Add files via upload

---
+++ 2.25/2.25/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+   
+    printf("PPPPPPPPP\n");
+    printf("    P   P\n");
+    printf("    P   P\n");
+    printf("    P   P\n");
+    printf("     P P \n");
+    printf("\n");
+   
+    printf("   JJ      \n");
+    printf("  J        \n");
+    printf(" J         \n");
+    printf("  J        \n");
+    printf("   JJJJJJJ \n");
+    printf("\n");
+
+  
+    printf("DDDDDDDDD\n");
+    printf("D       D\n");
+    printf("D       D\n");
+    printf(" D     D\n");
+    printf("   DDDD  \n");
+
+    return 0;
+}
